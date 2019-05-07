b192131ac3abc1d0b23fbdf9735d5719d6d48c63
Merlijn <24628@ma-web.nl>
2019-05-07 16:22:10 +0200

    fixed mine orbitcontrol bug with mouse movement

--- resources/views/metric/partials/chart.blade.php
+++ resources/views/metric/partials/chart.blade.php
@@ -2,6 +2,18 @@
     <div id="data_viz_cubes_update" style="width: 500px;">
     </div>
     <div id="data_viz_data_information">
-
+        <h3>District: <span id="insert_text_city_name"></span></h3>
+        <table class="table">
+            <thead class="thead-dark">
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Value</th>
+                <th scope="col">Percentage</th>
+            </tr>
+            </thead>
+            <tbody id="tbody_data_viz_show_data">
+            </tbody>
+        </table>
     </div>
+
 </div>
