950504f689f9c4545676f7a8a6cdc3341014e938
Merlijn <24628@ma-web.nl>
2019-05-13 15:40:04 +0200

    add more features to story creating

---
+++ resources/views/metric/partials/shareStoryPartials/storyPoints.blade.php
@@ -0,0 +1,9 @@
+<p>List top needs <span onclick="add_points_to_story_function()" style="text-decoration: underline;">Click here to add</span></p>
+<div id="add_points_to_story" style="display: none">
+    <div class="row">
+        <div class="col-sm-12 col-md-6 col-lg-3" style="margin-bottom: 9px"><input class="form-control"  type="text" placeholder="Default input"></div>
+        <div class="col-sm-12 col-md-6 col-lg-3" style="margin-bottom: 9px"><input class="form-control"  type="text" placeholder="Default input"></div>
+        <div class="col-sm-12 col-md-6 col-lg-3" style="margin-bottom: 9px"><input class="form-control"  type="text" placeholder="Default input"></div>
+        <div class="col-sm-12 col-md-6 col-lg-3" style="margin-bottom: 9px"><input class="form-control"  type="text" placeholder="Default input"></div>
+    </div>
+</div>
