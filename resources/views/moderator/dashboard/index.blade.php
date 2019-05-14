6c2178c6f42629634fc62d63a60baeac8b5e3d82
Merlijn <24628@ma-web.nl>
2019-05-14 16:09:00 +0200

    cvs convertor to json

---
+++ resources/views/moderator/dashboard/index.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.master')
+
+@section('css')
+
+
+@endsection
+
+
+@section('jumbotron')
+
+@endsection
+
+
+@section('sidebar')
+
+@endsection
+
+
+@section('content')
+    <h1>Moderator</h1>
+    <input type="file" id="myDOMElementId" onchange="parse()">
+@endsection
+
+
+@section('js')
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+    <script src="{{asset('js/papaparse.min.js')}}"></script>
+    <script>
+
+        var data;
+
+        function parse() {
+            var file = document.getElementById('myDOMElementId').files[0];
+
+            Papa.parse(file, {
+                header: true,
+                dynamicTyping: true,
+                complete: function(results) {
+                    data = results.data;
+                    console.log("Finished:", results.data);
+                }
+            });
+        }
+    </script>
+@endsection
